3ad5b21d5eb9c17a99bece393e3b1882e4510bd8
etermaw <etermaw@gmail.com>
2022-05-01 19:37:29 +0200

    Alpha version

---
+++ command_parser.py
@@ -0,0 +1,158 @@
+import operator
+import random
+from typing import Tuple, List
+
+
+def roll(dices_to_roll: List[int], start_from_zero: bool = False) -> List[int]:
+    if start_from_zero:
+        return [random.randint(0, x-1) for x in dices_to_roll]
+    else:
+        return [random.randint(1, x) for x in dices_to_roll]
+
+
+def execute_dice_cmd(cmd: str) -> Tuple[int, List[int]]:
+    program = parse_dice_command(cmd)
+    return execute_expr(program)
+
+
+def execute_expr(program) -> Tuple[int, List[int]]:
+    stack = []
+    rolls_made = []
+    OPS = {
+        '+': operator.add,
+        '-': operator.sub,
+        '*': operator.mul,
+        '/': operator.floordiv
+    }
+
+    for op in program:
+        if isinstance(op, int):
+            stack.append(op)
+        elif isinstance(op, tuple):
+            dice_count, dice_limit = op
+            rolls = roll([dice_limit] * dice_count)
+            rolls_made += rolls
+            stack.append(sum(rolls))
+        else:
+            right = stack.pop()
+            left = stack.pop()
+            stack.append(OPS[op](left, right))
+
+    return stack[-1], rolls_made
+
+
+def parse_dice_command(cmd: str):
+    # CMD: EXPR
+    # EXPR: MUL_EXPR
+    # MUL_EXPR: ADD_EXPR [["*" | "/"] ADD_EXPR]*
+    # ADD_EXPR: PRIM [["+" | "-"] PRIM]*
+    # PRIM: NUMBER | ROLL_EXPR | "(" EXPR ")"
+    # ROLL_EXPR: NUMBER "d" NUMBER
+    # NUMBER: [0-9]+
+
+    tokens = tokenize_cmd(cmd)
+    prog = Parser().parse_cmd(tokens)
+    return prog
+
+
+class Parser:
+    def __init__(self):
+        self.tokens = None
+        self.pos = 0
+        self.parsed = None
+
+    def parse_cmd(self, tokens):
+        self.tokens = tokens
+        self.pos = 0
+        self.parsed = []
+
+        self.mul_expr()
+        return self.parsed
+
+    def mul_expr(self):
+        self.add_expr()
+
+        while self.look_ahead(0) in {"*", "/"}:
+            op = self.look_ahead(0)
+            self.advance()
+            self.add_expr()
+            self.emit(op)
+
+    def add_expr(self):
+        self.prim()
+
+        while self.look_ahead(0) in {"+", "-"}:
+            op = self.look_ahead(0)
+            self.advance()
+            self.prim()
+            self.emit(op)
+
+    def prim(self):
+        tok = self.look_ahead(0)
+
+        if tok == "(":
+            self.advance()
+            self.mul_expr()
+            self.consume(")")
+
+        elif self.look_ahead(1) == "d":
+            self.roll_expr()
+
+        else:
+            self.emit(self.number())
+
+    def roll_expr(self):
+        dices_num = self.number()
+        self.consume("d")
+        limit_num = self.number()
+
+        self.emit((dices_num, limit_num))
+
+    def number(self):
+        num = int(self.look_ahead(0))
+        self.advance()
+        return num
+
+    def look_ahead(self, n: int) -> str:
+        if self.pos + n < len(self.tokens):
+            return self.tokens[self.pos + n]
+        else:
+            return ""
+
+    def emit(self, op):
+        self.parsed.append(op)
+
+    def advance(self):
+        self.pos += 1
+
+    def consume(self, expected: str):
+        if self.look_ahead(0) != expected:
+            raise Exception()
+
+        self.advance()
+
+
+def tokenize_cmd(cmd: str):
+    tokens = []
+    for raw_part in cmd.split():
+        pos = 0
+
+        while pos < len(raw_part):
+            if raw_part[pos] in {'d', '-', '+', '*', '/', '(', ')'}:
+                tokens.append(raw_part[pos])
+                pos += 1
+
+            else:
+                num, pos = number(raw_part, pos)
+                tokens.append(num)
+
+    return tokens
+
+
+def number(s: str, pos: int):
+    start = pos
+
+    while pos < len(s) and s[pos].isnumeric():
+        pos += 1
+
+    return s[start:pos], pos
---
+++ main.py
@@ -0,0 +1,98 @@
+import re
+from typing import Optional
+import os
+import discord
+from discord.ext import commands
+
+from command_parser import roll, execute_dice_cmd
+
+roll_validator = re.compile(r'[0-9d+-/*/() ]+')
+dice_mod_validator = re.compile(r'[1-9][0-9]*[bp]')
+
+bot = commands.Bot(command_prefix="#")
+
+
+@bot.event
+async def on_ready():
+    print("Bot ready!")
+
+
+@bot.command()
+async def r(ctx: commands.Context, cmd: str):
+    query_author = ctx.author
+
+    if len(cmd) == 0:
+        return
+
+    if len(cmd) > 100:
+        await ctx.send(f'{query_author.mention} Roll command can have max 100 characters.')
+        return
+
+    if roll_validator.fullmatch(cmd) is None:
+        await ctx.send(f'{query_author.mention} Roll command can only contain: numbers, rolls in XdY format, +-*/(), space.')
+        return
+
+    try:
+        result, rolls = execute_dice_cmd(cmd)
+        await ctx.send(f'{query_author.mention} [{",".join(str(x) for x in rolls)}] {result}')
+
+    except:
+        await ctx.send(f'{query_author.mention} Failed to evaluate command!')
+
+
+@bot.command()
+async def rc(ctx: commands.Context, skill_val: int, mod: Optional[str] = None):
+    if skill_val > 100 or skill_val < 1:
+        await ctx.send(f"{ctx.author.mention} Invalid skill value!")
+        return
+
+    if mod is not None and dice_mod_validator.fullmatch(mod) is None:
+        await ctx.send(f"{ctx.author.mention} Invalid dice modifier!")
+        return
+
+    base_roll = roll([10, 10], start_from_zero=True)
+
+    if mod is None:
+        modifiers = []
+        mod = 0
+    else:
+        mod_count = int(mod[:-1])
+        modifiers = roll([10] * mod_count, start_from_zero=True)
+        mod = -1 if mod[-1] == 'b' else 1
+
+    units = base_roll[1]
+    all_rolls = [x * 10 + units for x in [base_roll[0], *modifiers]]
+    all_rolls = [100 if x == 0 else x for x in all_rolls]
+
+    if mod == 0:
+        final_roll = all_rolls[0]
+    elif mod == -1:
+        final_roll = min(all_rolls)
+    else:
+        final_roll = max(all_rolls)
+
+    if final_roll == 100:
+        result = "Fumble!"
+
+    elif skill_val < 50 and final_roll >= 96:
+        result = "Fumble!"
+
+    elif final_roll > skill_val:
+        result = "Fail"
+
+    else:
+        if final_roll == 1:
+            result = "Critical Success!"
+        elif final_roll <= (skill_val // 5):
+            result = "Extreme Success"
+        elif final_roll <= (skill_val // 2):
+            result = "Hard Success"
+        else:
+            result = "Success"
+
+    rolls = [t * 10 for t in [base_roll[0], *modifiers]] + [units]
+
+    await ctx.send(f"{ctx.author.mention} [{','.join(str(x) for x in rolls)}] {final_roll} {result}")
+
+
+bot.run(os.getenv("DISCORD_BOT_TOKEN"))
